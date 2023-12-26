8c158dad3bc7903582dd8c9c4b8a871476c61511
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-12-26 16:55:01 -0300

    adiciona prettier e eslint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,12 @@
-import express, { Request, Response, Express } from "express";
-import cors from "cors";
-const app: Express = express();
-const port = 3000;
-app.use(cors());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+import express, { Request, Response, Express } from 'express'
+import cors from 'cors'
+const app: Express = express()
+const port = 3000
+app.use(cors())
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port  ${port}`);
-});
+  console.log(`Example app listening on port  ${port}`)
+})
