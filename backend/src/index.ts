f4fb2cbcc83acfb53b982145f3ccf9efb114d011
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-12-26 16:45:10 -0300

    inicializa backend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, Express } from "express";
+import cors from "cors";
+const app: Express = express();
+const port = 3000;
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port  ${port}`);
+});
