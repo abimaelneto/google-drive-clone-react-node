c25bf3055f0611f2e6b69e3f840f176c1fc7016e
Abimael <abimael.oliveiraneto@gmail.com>
2023-12-28 00:36:45 -0300

    feat be/autenticacao (#8)
    * cria utils para tratamento de erros
    
    * adiciona migrações para adicionar usuários
    
    * adiciona seed com usuário admin
    
    * cria prisma client
    
    * adiciona dependencias
    
    * adiciona tipos para jwt e express
    
    * cria módulo de autenticação
    
    * usa módulo de autenticação
    
    * adiciona configurações ao compose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,37 @@
-import express, { Request, Response, Express } from 'express'
+import express, {
+  Request,
+  Response,
+  Express,
+  json,
+  NextFunction,
+} from 'express'
 import serverless from 'serverless-http'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import 'dotenv/config'
+import { AuthRouter } from './modules/auth/router'
+import { GlobalErrorHandler } from './utils/GlobalErrorHandler'
 
 const app: Express = express()
 const port = process.env.PORT
 app.use(cors())
+app.use(json())
+app.use(cookieParser())
+
+app.use('/api/v1/auth', AuthRouter)
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('hey there')
+  req.requestTime = new Date().toISOString()
+  // console.log(req.cookies);
+  next()
+})
+
+app.use(GlobalErrorHandler.main)
 
 app.listen(port, () => {
   console.log(`Example app listening on port  ${port}`)
