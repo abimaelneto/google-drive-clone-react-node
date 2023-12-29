c55cf84ec1dd4882b37f5248826f23988b2fb9b1
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-12-29 10:17:18 -0300

    adiciona pastas e arquivos ao seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  const admin = await prisma.user.create({
+  await prisma.user.create({
     data: {
       password: 'password',
       email: 'admin@admin.com',
@@ -10,8 +10,48 @@ async function main() {
       role: 'ADMIN',
     },
   })
-
-  console.log({ admin })
+  const folder1 = await prisma.fileNode.create({
+    data: {
+      name: 'folder1',
+      ownerEmail: 'admin@admin.com',
+      isFolder: true,
+    },
+  })
+  const folder2 = await prisma.fileNode.create({
+    data: {
+      name: 'folder2',
+      ownerEmail: 'admin@admin.com',
+      isFolder: true,
+      parentId: folder1.id,
+    },
+  })
+  await prisma.fileNode.createMany({
+    data: [
+      {
+        name: 'file1',
+        ownerEmail: 'admin@admin.com',
+        content: 'test',
+      },
+      {
+        name: 'file2',
+        ownerEmail: 'admin@admin.com',
+        content: 'test',
+        parentId: folder1.id,
+      },
+      {
+        name: 'file3',
+        ownerEmail: 'admin@admin.com',
+        content: 'test',
+        parentId: folder1.id,
+      },
+      {
+        name: 'file4',
+        ownerEmail: 'admin@admin.com',
+        content: 'test',
+        parentId: folder2.id,
+      },
+    ],
+  })
 }
 main()
   .then(async () => {
@@ -22,3 +62,27 @@ main()
     await prisma.$disconnect()
     process.exit(1)
   })
+
+// WITH RECURSIVE filesAndFolders AS (
+//   SELECT
+//     id,
+//     name,
+//     "parentId",
+//     "isFolder"
+//   FROM
+//     public."FileNode"
+//   WHERE
+//     name = 'folder2'
+//   UNION
+//     SELECT
+//       f.id,
+//       f.name,
+//       f."parentId",
+//       f."isFolder"
+//     FROM
+//       public."FileNode"  as f
+//     INNER JOIN filesAndFolders as ff ON ff.id = f."parentId"
+// ) SELECT
+//   *
+// FROM
+//   filesAndFolders;
