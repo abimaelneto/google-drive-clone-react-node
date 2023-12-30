f1cb3edba4d32544ad9f7eedfc85ca288dba14c9
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-12-30 19:01:34 -0300

    usa tema e router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,17 @@
-import { useEffect } from "react";
+import { PropsWithChildren, useEffect } from 'react'
+import './global.css'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme'
 
-function App() {
+function App({ children }: PropsWithChildren) {
   const test = async () => {
-    const res = await fetch("http://localhost:3000");
-    console.log(await res.text());
-  };
+    const res = await fetch('http://localhost:3000')
+    console.log(await res.text())
+  }
   useEffect(() => {
-    test();
-  });
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div>Example app</div>
-    </>
-  );
+    test()
+  })
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
 
-export default App;
+export default App
