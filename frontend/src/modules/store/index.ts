a9edc4d0d111d5786b92401c0b9cd022b85606b5
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-12-31 00:42:09 -0300

    remove arquivo antigo

--- frontend/src/modules/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { createSlice } from '@reduxjs/toolkit'
-
-export const authSlice = createSlice({
-  name: 'auth',
-  initialState: { user: null, token: null },
-  reducers: {},
-})
-
-export const authReducer = authSlice.reducer
