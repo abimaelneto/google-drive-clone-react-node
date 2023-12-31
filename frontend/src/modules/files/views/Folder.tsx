2bc8cab9f026d868d7dd0a39850549daf6f5fbd4
Abimael Neto <abimael.oliveiraneto@gmail.com>
2023-12-31 12:51:01 -0300

    simplifica views

--- frontend/src/modules/files/views/Folder.tsx
+++ frontend/src/modules/files/views/Folder.tsx
@@ -1,23 +1,4 @@
-import { useAppDispatch } from '@/store/hooks'
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { getFileNodesThunk } from '../store/thunks/get'
-
 import { BaseLayout } from '../layouts/Base'
 export const Folder = () => {
-  const dispatch = useAppDispatch()
-
-  const params = useParams()
-  const fetchFolder = async (nodeId: string) => {
-    if (!nodeId) return
-    try {
-      await dispatch(getFileNodesThunk(nodeId as string)).unwrap()
-    } catch (err) {
-      console.log(err)
-    }
-  }
-  useEffect(() => {
-    fetchFolder(params.fileNodeId as string)
-  }, [params.fileNodeId])
-  return <BaseLayout />
+  return <BaseLayout type="get" />
 }
