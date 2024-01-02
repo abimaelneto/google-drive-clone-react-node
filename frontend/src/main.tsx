6e5ec21f9a39eb4984257894858f0b7e0c08e4e9
Abimael Neto <abimael.oliveiraneto@gmail.com>
2024-01-02 15:59:02 -0300

    remove componente não utilizado

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,16 +6,13 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/index.ts'
-import { AuthProvider } from './contexts/Auth.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Provider store={store}>
-        <App>
-          <RouterProvider router={router} />
-        </App>
-      </Provider>
-    </AuthProvider>
+    <Provider store={store}>
+      <App>
+        <RouterProvider router={router} />
+      </App>
+    </Provider>
   </React.StrictMode>
 )
