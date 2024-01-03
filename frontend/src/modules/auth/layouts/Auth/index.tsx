5d1c3dd5b2e6e5b640967d18625e3db7cdf9e901
Abimael Neto <abimael.oliveiraneto@gmail.com>
2024-01-02 21:28:51 -0300

    redireciona para login

--- frontend/src/modules/auth/layouts/Auth/index.tsx
+++ frontend/src/modules/auth/layouts/Auth/index.tsx
@@ -1,7 +1,18 @@
 import { Stack } from '@mui/material'
+import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import { useLocation, useNavigate } from 'react-router-dom'
+
 export const AuthLayout = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (!['/login', '/register'].includes(location.pathname)) {
+      navigate('/login')
+    }
+  }, [])
   return (
     <Stack
       sx={{ width: '100vw', height: '100vh', background: 'lightgrey' }}
